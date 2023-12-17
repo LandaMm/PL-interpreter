e6d142e39c3c67d3072506e4a298e014c0c41d13
LandaMm <qwer.009771@gmail.com>
2023-12-17 18:19:20 +0100

    feat: string & array value stringify support

--- src/builtin/stringify.rs
+++ src/builtin/stringify.rs
@@ -1,5 +1,6 @@
 use crate::{
-    BoolValue, DecimalValue, FunctionValue, IntegerValue, NativeFnValue, RuntimeValue, ValueType,
+    ArrayValue, BoolValue, DecimalValue, FunctionValue, IntegerValue, NativeFnValue, RuntimeValue,
+    StringValue, ValueType,
 };
 
 use super::cast_value;
@@ -36,6 +37,31 @@ pub fn stringify(value: Box<dyn RuntimeValue>) -> String {
         ValueType::NativeFn => {
             let function = cast_value::<NativeFnValue>(&value).unwrap();
             format!("<native-function {}>", function.name,)
+        }
+        ValueType::String => {
+            let string = cast_value::<StringValue>(&value).unwrap();
+            format!("{}", string.value())
+        }
+        ValueType::Array => {
+            let array = cast_value::<ArrayValue>(&value).unwrap();
+            format!(
+                "[{}]",
+                array
+                    .value()
+                    .into_iter()
+                    .map(|x| {
+                        if x.lock().unwrap().kind() == ValueType::String {
+                            format!(
+                                "\"{}\"",
+                                stringify(dyn_clone::clone_box(&**x.lock().unwrap()))
+                            )
+                        } else {
+                            stringify(dyn_clone::clone_box(&**x.lock().unwrap()))
+                        }
+                    })
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            )
         }
     }
 }
