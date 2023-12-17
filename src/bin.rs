f00a3699222c15a6e541865c568407759e3c3695
LandaMm <qwer.009771@gmail.com>
2023-12-17 18:19:11 +0100

    feat: dynamic file

--- src/bin.rs
+++ src/bin.rs
@@ -1,20 +1,27 @@
-use std::{fs, thread};
+use std::{env, fs, thread};
 
 use pl_ast::Parser;
 use pl_interpreter::{setup_environment, Interpreter};
 
 fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let file_name = if args.len() > 1 {
+        args.get(1).unwrap().to_owned()
+    } else {
+        "test/main.amr".to_string()
+    };
+
     thread::Builder::new()
         .stack_size(1024 * 1024)
         .spawn(|| {
-            let source = fs::read_to_string("test/main.pl").unwrap();
+            let source = fs::read_to_string(file_name).unwrap();
             let mut parser = Parser::from_source(source);
             let ast = parser.produce_ast().unwrap();
             let mut interpreter = Box::new(Interpreter::new());
 
             let env_id = setup_environment();
 
-            match interpreter.run(Box::new(ast), env_id, false) {
+            match interpreter.run(Box::new(ast), env_id) {
                 Ok(res) => res,
                 Err(err) => panic!("{}", err),
             };
