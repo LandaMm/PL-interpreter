1a275fc371158c4c557f833b462dbf3e55802237
LandaMm <qwer.009771@gmail.com>
2023-12-17 18:19:58 +0100

    feat: use * from string & arrays

--- src/values/mod.rs
+++ src/values/mod.rs
@@ -2,19 +2,23 @@ use std::{any::Any, fmt::Debug};
 
 use dyn_clone::DynClone;
 
+mod array;
 mod bool;
 mod decimal;
 mod function;
 mod integer;
 mod native_fn;
 mod null;
+mod string;
 
+pub use array::*;
 pub use bool::*;
 pub use decimal::*;
 pub use function::*;
 pub use integer::*;
 pub use native_fn::*;
 pub use null::*;
+pub use string::*;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum ValueType {
@@ -24,6 +28,8 @@ pub enum ValueType {
     Boolean,
     NativeFn,
     Function,
+    String,
+    Array,
 }
 
 pub trait RuntimeValue: DynClone + Debug + Send + Sync {
