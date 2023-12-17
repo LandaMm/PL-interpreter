654a32bf55fe25d50b4d892fd69e168fb3b536b6
LandaMm <qwer.009771@gmail.com>
2023-12-17 21:44:35 +0100

    feat: float rounding math methods

--- src/builtin/math.rs
+++ src/builtin/math.rs
@@ -4,7 +4,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use crate::{DecimalValue, Key, RuntimeValue, Value};
+use crate::{cast_value, DecimalValue, Key, NullValue, RuntimeValue, Value, ValueType};
 
 use super::{mk_native_fn, mk_runtime_value};
 
@@ -14,6 +14,90 @@ fn random(_args: Vec<Arc<Mutex<Box<dyn RuntimeValue>>>>) -> Arc<Mutex<Box<dyn Ru
     let num = rng.gen::<f64>();
 
     mk_runtime_value(Box::new(DecimalValue::from(num)))
+}
+
+fn floor(args: Vec<Arc<Mutex<Box<dyn RuntimeValue>>>>) -> Arc<Mutex<Box<dyn RuntimeValue>>> {
+    if args.is_empty() {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    let arg = args.get(0).unwrap();
+    let value = arg.lock().unwrap();
+
+    if value.kind() != ValueType::Decimal && value.kind() != ValueType::Integer {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    if value.kind() == ValueType::Decimal {
+        let decimal = cast_value::<DecimalValue>(&value).unwrap();
+        return mk_runtime_value(Box::new(DecimalValue::from(decimal.value().floor())));
+    }
+
+    // only integer case left
+    return arg.clone();
+}
+
+fn ceil(args: Vec<Arc<Mutex<Box<dyn RuntimeValue>>>>) -> Arc<Mutex<Box<dyn RuntimeValue>>> {
+    if args.is_empty() {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    let arg = args.get(0).unwrap();
+    let value = arg.lock().unwrap();
+
+    if value.kind() != ValueType::Decimal && value.kind() != ValueType::Integer {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    if value.kind() == ValueType::Decimal {
+        let decimal = cast_value::<DecimalValue>(&value).unwrap();
+        return mk_runtime_value(Box::new(DecimalValue::from(decimal.value().ceil())));
+    }
+
+    // only integer case left
+    return arg.clone();
+}
+
+fn trunc(args: Vec<Arc<Mutex<Box<dyn RuntimeValue>>>>) -> Arc<Mutex<Box<dyn RuntimeValue>>> {
+    if args.is_empty() {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    let arg = args.get(0).unwrap();
+    let value = arg.lock().unwrap();
+
+    if value.kind() != ValueType::Decimal && value.kind() != ValueType::Integer {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    if value.kind() == ValueType::Decimal {
+        let decimal = cast_value::<DecimalValue>(&value).unwrap();
+        return mk_runtime_value(Box::new(DecimalValue::from(decimal.value().trunc())));
+    }
+
+    // only integer case left
+    return arg.clone();
+}
+
+fn round(args: Vec<Arc<Mutex<Box<dyn RuntimeValue>>>>) -> Arc<Mutex<Box<dyn RuntimeValue>>> {
+    if args.is_empty() {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    let arg = args.get(0).unwrap();
+    let value = arg.lock().unwrap();
+
+    if value.kind() != ValueType::Decimal && value.kind() != ValueType::Integer {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    if value.kind() == ValueType::Decimal {
+        let decimal = cast_value::<DecimalValue>(&value).unwrap();
+        return mk_runtime_value(Box::new(DecimalValue::from(decimal.value().round())));
+    }
+
+    // only integer case left
+    return arg.clone();
 }
 
 pub fn get_math() -> HashMap<Key, Value> {
@@ -29,6 +113,38 @@ pub fn get_math() -> HashMap<Key, Value> {
         mk_native_fn(
             "math.random".to_string(),
             Arc::new(Mutex::new(Box::new(random))),
+        ),
+    );
+
+    map.insert(
+        "floor".to_string(),
+        mk_native_fn(
+            "math.floor".to_string(),
+            Arc::new(Mutex::new(Box::new(floor))),
+        ),
+    );
+
+    map.insert(
+        "ceil".to_string(),
+        mk_native_fn(
+            "math.ceil".to_string(),
+            Arc::new(Mutex::new(Box::new(ceil))),
+        ),
+    );
+
+    map.insert(
+        "trunc".to_string(),
+        mk_native_fn(
+            "math.trunc".to_string(),
+            Arc::new(Mutex::new(Box::new(trunc))),
+        ),
+    );
+
+    map.insert(
+        "round".to_string(),
+        mk_native_fn(
+            "math.round".to_string(),
+            Arc::new(Mutex::new(Box::new(round))),
         ),
     );
 
