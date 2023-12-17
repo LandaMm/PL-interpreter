25d15929cd170bfc121d19784e06ac02679bf2da
LandaMm <qwer.009771@gmail.com>
2023-12-17 20:30:53 +0100

    feat: objectc value

---
+++ src/values/object.rs
@@ -0,0 +1,40 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use super::{RuntimeValue, ValueType};
+
+pub type Key = String;
+pub type Value = Arc<Mutex<Box<dyn RuntimeValue>>>;
+
+#[derive(Debug, Clone)]
+pub struct ObjectValue {
+    kind: ValueType,
+    map: HashMap<Key, Value>,
+}
+
+impl RuntimeValue for ObjectValue {
+    fn kind(&self) -> ValueType {
+        self.kind
+    }
+
+    fn into_any(&self) -> Box<dyn std::any::Any> {
+        Box::new(dyn_clone::clone(self))
+    }
+}
+
+impl From<HashMap<Key, Value>> for ObjectValue {
+    fn from(map: HashMap<Key, Value>) -> Self {
+        Self {
+            kind: ValueType::Object,
+            map,
+        }
+    }
+}
+
+impl ObjectValue {
+    pub fn map(&self) -> HashMap<Key, Value> {
+        self.map.clone()
+    }
+}
