263b71e85b8243752402d8c4f78fb2f3853e37e7
LandaMm <qwer.009771@gmail.com>
2023-12-20 15:27:44 +0100

    feat: native typeof func

---
+++ src/builtin/type_of.rs
@@ -0,0 +1,27 @@
+use std::sync::{Arc, Mutex};
+
+use crate::{NullValue, RuntimeValue, StringValue, ValueType};
+
+use super::mk_runtime_value;
+
+pub fn native_type_of(
+    args: Vec<Arc<Mutex<Box<dyn RuntimeValue>>>>,
+) -> Arc<Mutex<Box<dyn RuntimeValue>>> {
+    if args.is_empty() {
+        return mk_runtime_value(Box::new(NullValue::default()));
+    }
+
+    let arg = args.get(0).unwrap().lock().unwrap();
+
+    let value_type: String = match arg.kind() {
+        ValueType::Array => "array".into(),
+        ValueType::Boolean => "boolean".into(),
+        ValueType::Decimal | ValueType::Integer => "number".into(),
+        ValueType::Function | ValueType::NativeFn => "function".into(),
+        ValueType::Null => "null".into(),
+        ValueType::Object => "object".into(),
+        ValueType::String => "string".into(),
+    };
+
+    mk_runtime_value(Box::new(StringValue::from(value_type)))
+}
